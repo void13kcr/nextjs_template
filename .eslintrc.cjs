3926b441b7f72447a09ce055e9947d4f6812975f
yudev116 <dev.101.yu@icloud.com>
2024-10-05 21:02:02 +0900

    add: eslint, prettier追加

---
+++ .eslintrc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  ignorePatterns: ['.eslintrc.cjs'],
+  extends: ['eslint:recommended', 'plugin:import/recommended', 'next/core-web-vitals', 'next/typescript', 'prettier'],
+  root: true,
+};
